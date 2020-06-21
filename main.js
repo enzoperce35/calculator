e4e7ea3449e0d87d9ae08035aa036208d0bd93cd
enzoperce35 <enzoperce35@gmail.com>
2020-06-22 05:59:33 +0800

    activated the history button

--- main.js
+++ main.js
@@ -25,6 +25,8 @@ function operator(num1, operator, num2) {
 
 
 
+
+
 var strArray = [];
 function processNumSentence(numString) {
 const decString = (string) => string.slice(string.length-1) == '.';
@@ -56,7 +58,6 @@ if (negative) {
 if (numString.includes('.')) {        
       decCount = numString.match(/\./g).length;
       numArray = numString.match(/-?x?[0-9]+[\.+*-/=]/g);
-
 for (i=0; i<decCount; i++) {
   decIndex = numArray.findIndex(decString);
       strArray.push(numArray[decIndex]);
@@ -79,6 +80,7 @@ numArray.forEach(function(r) {
 }
     return numArray;
 };
+
 
 
 
@@ -121,79 +123,145 @@ operator(num1, mathSign, num2);
 
 
 
-
-var newString = '';
-var finalArray = [];
-const decimal = document.getElementById('dec');
-const equal = document.getElementById('equals');
 const input = document.getElementById('input');
 input.value;
+var finalArray = []; 
+const equal = document.getElementById('equals');
+const decimal = document.getElementById('dec');
 
-
-
-const buttons = document.querySelectorAll('button:not(#clear)');
-buttons.forEach((b) => b.addEventListener('click', function (i) { 
-
-//create proper input
-input.value += b.value;
-
-//avoid irregular operation decimal input
-newString += b.value;
-if (b.value.match(/[+*/-]/) || input.value == '') newString = '';               
-decimal.disabled = (newString.match(/\./))? true:false;
-
-//fix decimal input with no integer
-if (b.value == '.' && !input.value.substr(input.value.lastIndexOf('.')-1, 1).match(/[0-9]/))         
-         input.value = input.value.replace(/.$/, '0.');
-
-
-//avoid irregular operation with equal sign
-(!input.value.match(/[0-9]+[+*/-](\(-)?[0-9]+/g))? document.getElementById('equals').value = '':document.getElementById('equals').value = '=';
-
-//avoid irregular operation with negative values
-if (input.value.match(/\(-b/g)) input.value = input.value.substr(0, input.value.indexOf('('));
-(newString.slice(-1).match(/(\d|\.)/) || newString.slice(-1) == 'b')? document.getElementById('neg').value = '': document.getElementById('neg').value = '(-';
-
-//avoid irregular operation with operator values
-if (input.value[0].match(/[+*/\-=]/)) input.value = '';
+var newString = '';
+function createInput(mathStr, mathInp) {
   
 
-//avoid duplication
-if (input.value.match(/[*+/-][*+/-]/)) input.value = input.value.substr(0, input.value.length-1);          
-if (input.value.match(/(\(-)(\(-)/)) input.value = input.value.substr(0, input.value.length-2);          
+//avoid irregular operation decimal input
+newString += mathInp;
+if (mathInp.match(/[+*/-]/) || mathStr == '') newString = '';               
+decimal.disabled = (newString.match(/\./))? true:false;
 
+
+//fix decimal input with no integer
+if (mathInp == '.' && !mathStr.substr(mathStr.lastIndexOf('.')-1, 1).match(/[0-9]/))         
+         input.value  = mathStr.replace(/.$/, '0.');
+   
+//avoid irregular operation with equal sign
+(!mathStr.match(/[0-9]+[+*/-](\(-)?[0-9]+/g))? document.getElementById('equals').value = '':document.getElementById('equals').value = '=';
+
+
+//avoid irregular operation with negative values
+if (mathStr.match(/\(-b/g)) input.value = mathStr.substr(0, mathStr.indexOf('(')); mathStr = input.value;
+(newString.slice(-1).match(/(\d|\.)/) || newString.slice(-1) == 'b')? document.getElementById('neg').value = '': document.getElementById('neg').value = '(-';
+
+
+//avoid irregular operation with operator values
+if (mathStr[0].match(/[+*/\-=]/)) input.value = '';
+
+//avoid duplication
+if (mathStr.match(/[*+/-][*+/-]/)) input.value = mathStr.substr(0, mathStr.length-1);         
+if (mathStr.match(/(\(-)(\(-)/)) input.value = mathStr.substr(0, mathStr.length-2); 
 
 //backspace support
-if (input.value.includes('b')) input.value = input.value.substr(0, input.value.length-2);
+if (mathStr.includes('b')) input.value = mathStr.substr(0, mathStr.length-2);
 
 
 
-
-
-
-//convert string value to an array of values...
+//convert to array
 mathPhrase = processNumSentence(input.value);
 
-
 //cut the values to proper values
-if (b.value == '=') { 
-processCount = mathPhrase.length -1;
+if (mathInp == '=') { 
+    processCount = mathPhrase.length -1;
+     historyA = input.value;
+
                                         
-mathPhrase.forEach(function(i) {
+  mathPhrase.forEach(function(i) {
       finalArray.push(i.substr(0, i.length -1));
       finalArray.push(i.substr(-1));
 });
 
 
-
-
 //ommit the equal sign then proceed to the math process
 finalArray.pop();
 input.value = processMath(finalArray, processCount);
+
+historyB = input.value;
+var results = historyA + ',' + historyB;
+save(results);
 };
+
+
+document.getElementById('clear').addEventListener('click', (c) => window.location.reload());
+document.getElementById('neg').addEventListener('click', (c) => input.value.concat(document.getElementById('neg').value));
+
+}
+
+
+
+
+
+
+//save computation history
+var historyFiles = JSON.parse(window.localStorage.getItem('user')) || [];
+function save(file){
+a = file.substr(0, file.indexOf(','));
+b = file.substr(file.indexOf(',')+1);
+
+    var result = {
+    equation: a,
+    result: b,
+};
+historyItem = result;
+historyFiles.push(historyItem);
+window.localStorage.setItem('user', JSON.stringify(historyFiles));
+}
+
+
+//create history list
+var histCont = document.getElementById('historyList');
+function convertToList(array){
+
+   array.forEach(function(i){
+
+     var hist = document.createElement('p');
+         hist.textContent = i.equation + ' ' +  i.result;
+         hist.setAttribute('style', 'font-style: italic; cursor: pointer');
+         hist.id = 'historyItem';
+         histCont.insertBefore(hist, histCont.childNodes[0]);
+         hist.addEventListener('click', () => input.value = i.equation.substr(0, i.equation.indexOf('=')));
+
+    if (histCont.childElementCount > 10) histCont.removeChild(histCont.childNodes[10]);
+
+ });
+};
+
+
+//show/hide history list
+var z = 0;
+document.getElementById('history').addEventListener('click', function(e) {
+    x=['show', 'hide'];
+
+    e = x[z++];
+    if (z==2) z = 0;
+
+if(e == 'show') convertToList(historyFiles);
+if(e == 'hide') document.querySelectorAll('p').forEach((i) => i.remove());
     
-    //additional buttons
-    document.getElementById('clear').addEventListener('click', (c) => window.location.reload());
-    document.getElementById('neg').addEventListener('click', (c) => input.value.concat(document.getElementById('neg').value));
-    
-}))
+});
+
+
+
+
+
+
+
+
+
+input.addEventListener('keyup', (e) => createInput(input.value, e.key));
+
+const buttons = document.querySelectorAll('button:not(#clear):not(#history');
+buttons.forEach((b) => b.addEventListener('click', function(e) {
+    input.value += b.value;
+    createInput(input.value, b.value);
+}));
+
+
+
